5484bf8f352e2e160dec3e5559135ac03f40bef9
vladyslav.Ievdokymov <vladyslav.ievdokymov@onicore.io>
2025-10-21 13:53:57 +0300

    upd desc

--- src/definitions.ts
+++ src/definitions.ts
@@ -2,7 +2,7 @@ export interface CapacitorAppStoreInfoPlugin {
   /**
    * Returns information about the app environment:
    * - isTestFlight: true if the app is running in TestFlight
-   * - storeCountry: the App Store country code (e.g., "US")
+   * - storeCountry: the App Store country code (e.g., "USA")
    */
   getInfo(): Promise<{
     isTestFlight: boolean;
